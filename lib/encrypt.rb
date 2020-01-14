822a924f92026098f71eb9ab25727c706801606c
Chase.Gaddis <cgaddis36@gmail.com>
2020-01-14 16:38:29 -0700

    Implemented correct use of arguments in command line interface for encrypt.rb

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,13 +1,13 @@
 require './lib/enigma.rb'
 require 'date'
 
-  file_to_encrypt = File.open('./lib/message.txt', 'r')
-  encrypted_file = File.open('./lib/encrypted.txt', 'w')
+  file_to_encrypt = File.open(ARGV[0], 'r')
+  encrypted_file = File.open(ARGV[1], 'w')
+  read_encrypted_file = File.open(ARGV[1], 'r')
   enigma = Enigma.new
-
-  ARGV == ['./lib/message.txt', './lib/encrypted.txt']
 
   read_file = file_to_encrypt.read
   encrypted_file.write(enigma.encrypt(read_file.gsub("\n","")))
+  encrypted_file.close
 
   p "created 'encrypted.txt' with the key #{enigma.key.join} and date #{enigma.date}"
