94211c77b655cc82df16a907bc996bbf78ed2818
Chase.Gaddis <cgaddis36@gmail.com>
2020-01-14 16:12:29 -0700

    Working on cleaning up command line intercface for final submission.

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,12 +1,13 @@
 require './lib/shift.rb'
+require './lib/enigma.rb'
 require 'date'
 
   file_to_encrypt = File.open('./lib/message.txt', 'r')
   encrypted_file = File.open('./lib/encrypted.txt', 'w')
   shift = Shift.new
+  enigma = Enigma.new
 
   ARGV == ['./lib/message.txt', './lib/encrypted.txt']
-
 
   read_file = file_to_encrypt.read
   encrypted_file.write(shift.encrypt_hash(read_file.gsub("\n","")))
